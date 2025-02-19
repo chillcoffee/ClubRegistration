9a9cbe61bca22f74b45863661db85f6258fef7b3
Ruffa <ruffaresentes@me.com>
2025-02-19 14:46:38 +0800

    add displayList, register methods

--- ClubRegistration/ClassRegistrationQuery.cs
+++ ClubRegistration/ClassRegistrationQuery.cs
@@ -24,6 +24,26 @@ namespace ClubRegistration
         public string _FirstName, _MiddleName, _LastName, _Gender, _Program;
         public int _Age;
 
+        public ClassRegistrationQuery()
+        {
+            connectionString = "Data Source=RS\\SQLSTUDENTS;Initial Catalog=Lab2Resentes;Integrated Security=True";
+            sqlConnect = new SqlConnection(connectionString);
+        }
 
+        public bool DisplayList()
+        {
+            string ViewClubMembers = "SELECT * FROM ClubMembers;";
+            sqlAdapter = new SqlDataAdapter(ViewClubMembers, sqlConnect);
+            dataTable.Clear();
+            sqlAdapter.Fill(dataTable);
+            bindingSource.DataSource = dataTable;
+            return true;
+        }
+
+        public bool RegisterStudent(int ID, long StudentID, string FirstName, string MiddleName, string LastName, int Age, string Gender, string Program)
+        {
+            sqlCommand = new SqlCommand("INSERT INTO ClubMembers VALUES(@ID, @StudentID, @FirstName, @MiddleName, @LastName, @Age, @Gender, @Program)", sqlConnect); sqlCommand.Parameters.Add("@ID", SqlDbType.Int).Value = ID; sqlCommand.Parameters.Add("@RegistrationID", SqlDbType.BigInt).Value = StudentID; sqlCommand.Parameters.Add("@StudentID", SqlDbType.VarChar).Value = StudentID; sqlCommand.Parameters.Add("@FirstName", SqlDbType.VarChar).Value = FirstName; sqlCommand.Parameters.Add("@MiddleName", SqlDbType.VarChar).Value = MiddleName; sqlCommand.Parameters.Add("@LastName", SqlDbType.VarChar).Value = LastName; sqlCommand.Parameters.Add("@Age", SqlDbType.Int).Value = Age; sqlCommand.Parameters.Add("@Gender", SqlDbType.VarChar).Value = Gender; sqlCommand.Parameters.Add("@Program", SqlDbType.VarChar).Value = Program;
+            sqlConnect.Open(); sqlCommand.ExecuteNonQuery(); sqlConnect.Close(); return true;
+        }
     }
 }
